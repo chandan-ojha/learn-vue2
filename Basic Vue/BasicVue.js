2947721412cc1fba3e8379c19c8e4dae7c9fc760
Chandan Ojha <chandanojha776@gmail.com>
2022-10-12 14:21:26 +0600

    Handling User Input Practice Complete

--- Basic Vue/BasicVue.js
+++ Basic Vue/BasicVue.js
@@ -10,7 +10,7 @@ var app2 = new Vue({
 var app3 = new Vue({
     el:'#app-3',
     data: {
-        seen : false
+        seen : true
     }
 })
 
@@ -23,5 +23,26 @@ var app4 = new Vue({
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
         ]
+    }
+})
+
+/*For Handling User Input*/
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function (){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Write Something..!'
     }
 })
