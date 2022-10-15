e41a68c2fdf9c66b18abdccfb13591840f6e1ccc
Chandan Ojha <chandanojha776@gmail.com>
2022-10-15 14:51:18 +0600

    superstore project basic setup done

---
+++ superstore/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css'
+/*jQuery*/
+import $ from 'jquery'
+/*Bootstrap*/
+import 'bootstrap'
+
+createApp(App).mount('#app')
