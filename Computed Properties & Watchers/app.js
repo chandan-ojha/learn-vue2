b734788d67e13d329282f2af809a356d8ce608f1
Chandan Ojha <chandanojha776@gmail.com>
2022-10-15 11:20:47 +0600

    Watchers Practice Complete

--- Computed Properties & Watchers/app.js
+++ Computed Properties & Watchers/app.js
@@ -32,3 +32,39 @@ var vm = new Vue({
         }
     }
 })
+
+/*For Watchers*/
+var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+        question: '',
+        answer: 'I cannot give you an answer until you ask a question!'
+    },
+    watch: {
+        // whenever question changes, this function will run
+        question: function (newQuestion, oldQuestion) {
+            this.answer = 'Waiting for you to stop typing...'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function () {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+        getAnswer: function () {
+            if (this.question.indexOf('?') === -1) {
+                this.answer = 'Questions usually contain a question mark. ;-)'
+                return
+            }
+            this.answer = 'Thinking...'
+            var vm = this
+            axios.get('https://yesno.wtf/api')
+                .then(function (response) {
+                    vm.answer = _.capitalize(response.data.answer)
+                })
+                .catch(function (error) {
+                    vm.answer = 'Error! Could not reach the API. ' + error
+                })
+        }
+    }
+})
