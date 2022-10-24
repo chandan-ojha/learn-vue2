ff1b9964ea1df35ba09272b160259757a745fe6e
Chandan Ojha <chandanojha776@gmail.com>
2022-10-24 15:48:18 +0600

    inventory layout design updated

--- Superstore Project/src/components/views/Inventory.vue
+++ Superstore Project/src/components/views/Inventory.vue
@@ -1,19 +1,21 @@
 <template>
   <!--Bootstrap Card-->
   <div v-if="!loading" class="row g-2">
-    <div v-for="(item,index) in items" :key="index" class="card" style="width: 13rem;">
-      <router-link tag="div" :to="{ path: '/item/' + item.id}">
-        <img :src="item.photo" class="card-img-top" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title text-center">{{ item.title }}</h5>
-        </div>
-      </router-link>
+    <div v-for="(item,index) in items" :key="index" class="col-lg-3">
+      <div class="card">
+        <router-link tag="div" :to="{ path: '/item/' + item.id}">
+          <img :src="item.photo" class="card-img-top" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title text-center">{{ item.title }}</h5>
+          </div>
+        </router-link>
        <div class="card-footer">
           <span class="card-text">${{ item.price }}</span>
           <a @click="addToCart(item)" class="btn btn-sm btn-primary float-end">+ add</a>
         </div>
       </div>
     </div>
+  </div>
   <h1 v-else>Loading...</h1>
 </template>
 
