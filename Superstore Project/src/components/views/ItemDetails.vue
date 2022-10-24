58a14acf790a729267a47dadaa498a469fb443f4
Chandan Ojha <chandanojha776@gmail.com>
2022-10-24 14:29:05 +0600

    Fetching Item Details

--- Superstore Project/src/components/views/ItemDetails.vue
+++ Superstore Project/src/components/views/ItemDetails.vue
@@ -1,10 +1,37 @@
 <template>
-   <h3>Details {{ $route.params.id }}</h3>
+  <!--<h3>Details {{ $route.params.id }}</h3>-->
+  <div v-if="item" class="row">
+    <div class="col-sm-6">
+      <img :src="item.photo" alt="Photo">
+    </div>
+    <div class="col-sm-6">
+      <h4>{{ item.title }}</h4>
+      <p>{{ item.description }}</p>
+      <p>{{ item.price }}</p>
+    </div>
+  </div>
+  <h3 v-else>Loading...</h3>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return {
+      item: null
+    }
+  },
+  mounted() {
+    this.fetchItem()
+  },
+  methods: {
+    fetchItem(){
+      var self = this;
+      axios.get('http://localhost:3000/item/'+ this.$route.params.id).then(response => {
+        self.item = response.data
+      })
+    }
+  }
 }
 </script>
 
